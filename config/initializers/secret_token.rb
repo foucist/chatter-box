b1fe3120b0f958da60152fa4eba35cdab2ea5dac
James Robey <james.robey@gmail.com>
2012-04-10 14:29:38 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatterbox::Application.config.secret_token = '3d539450557e69b927bf6d23c45aef32ccf09b56e37810b9cc064ed31d28e908d677e7d7bb2d00bf0f3127d3772aadd0edeeffd5ddd76e3a58c6bd96e249408e'
