0ad1af70c4f89673360bbcaa8fc2ab62c9015440
James Robey <james.robey@gmail.com>
2012-04-11 18:21:05 +0700

    updated db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Discussion.create(program_id: 1, user_id: 1, title: "Least likeable character?", deleted: false)
-Comment.create(user_id: 1, discussion_id: 1, body: "Dom gets my vote. Just did not like him at all.", deleted: false)
-Comment.create(user_id: 1, discussion_id: 1, body: "The season Ari was abusing Lloyd, I found Ari to be totally dispicable. He mellowed later, though, and I suppose Ari apologists would argue that he acted like a total prick for Lloyd's own good. I don't agree.", deleted: false)
-Comment.create(user_id: 1, discussion_id: 1, body: "Mandy Moore...", deleted: false)
-Comment.create(user_id: 1, discussion_id: 1, body: "I didn't like her either!", deleted: false)
-Comment.create(user_id: 1, discussion_id: 1, body: "Easily Ashley. Easily.", deleted: false)
 
+
+Discussion.create(program_id: 1, user_id: 1, title: 'Do you think Aum looks hot in jeans?')
+Discussion.create(program_id: 1, user_id: 1, title: 'How should the ending story be?')
+Discussion.create(program_id: 1, user_id: 2, title: 'Why did Reya hit her mother?')
+Discussion.create(program_id: 2, user_id: 3, title: 'Can we use ChatterBox to win the prize?')
+Discussion.create(program_id: 2, user_id: 4, title: 'Is this gameshow too easy?')
+
+Comment.create(discussion_id: 1, user_id: 1, body: 'Hell yeah! hot and sexy!')
+Comment.create(discussion_id: 1, user_id: 2, body: 'Are you kidding? it\'s surgery man..')
+Comment.create(discussion_id: 1, user_id: 3, body: 'look like ladyboy..but skinnier')
+Comment.create(discussion_id: 1, user_id: 4, body: 'I like skirt more')
+Comment.create(discussion_id: 1, user_id: 1, body: 'totally agree!')
+
+Discussion.create(program_id: 1, user_id: 1, title: "Least likeable character?")
+
+Comment.create(user_id: 1, discussion_id: 6, body: "Dom gets my vote. Just did not like him at all.")
+Comment.create(user_id: 2, discussion_id: 6, body: "The season Ari was abusing Lloyd, I found Ari to be totally dispicable. He mellowed later, though, and I suppose Ari apologists would argue that he acted like a total prick for Lloyd's own good. I don't agree.")
+Comment.create(user_id: 3, discussion_id: 6, body: "Mandy Moore...")
+Comment.create(user_id: 1, discussion_id: 6, body: "I didn't like her either!")
+Comment.create(user_id: 4, discussion_id: 6, body: "Easily Ashley. Easily.")
