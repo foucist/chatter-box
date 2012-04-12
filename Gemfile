2732887621f431916549dda4b2c2230da07b936c
James Robey <james.robey@gmail.com>
2012-04-12 16:48:16 +0700

    added image uploading support.  user can add image and get a thumbnail in /users/edit

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'omniauth-facebook'
 #gem 'omniauth-twitter'
 #gem 'omniauth-google'
 
+gem 'carrierwave'
+gem 'mini_magick'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120410105010) do
+ActiveRecord::Schema.define(:version => 20120412090100) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20120410105010) do
     t.integer  "current_program_id"
     t.datetime "created_at",                                            :null => false
     t.datetime "updated_at",                                            :null => false
+    t.string   "image"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
