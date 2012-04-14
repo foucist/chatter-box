9970378cc9d3e4596430d89c61d2ddf9500e636a
James Robey <james.robey@gmail.com>
2012-04-14 20:17:36 +0700

    minor commit, just getting checkin model/db in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120414101045) do
+ActiveRecord::Schema.define(:version => 20120414131637) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(:version => 20120414101045) do
     t.string   "uid"
     t.string   "token"
     t.string   "secret"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "checkins", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "program_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
